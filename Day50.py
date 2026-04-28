8b9f202a13575934354fed3dfe4a677e1cd0906f
Hmnsdixit <hmnsdixit@gmail.com>
2026-04-28 20:40:04 +0530

    Day 50: Kubernetes Deployment of AI App

---
+++ Day50.py
@@ -0,0 +1,17 @@
+"""
+Day 50: Kubernetes Deployment
+
+What I did:
+- Used Dockerized FastAPI app
+- Created Kubernetes Deployment
+- Created Kubernetes Service
+- Deployed app on cluster
+
+Commands:
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+
+Check:
+kubectl get pods
+kubectl get services
+"""
